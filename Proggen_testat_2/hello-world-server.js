bb6d2ea30008faae9f4513ce0ab5616cd1e00e66
BKMaster94 <bruno.branco2@web.de>
2017-06-12 18:36:19 +0200

    Request ist da :D
    WUHU

--- Proggen_testat_2/hello-world-server.js
+++ Proggen_testat_2/hello-world-server.js
@@ -1,9 +1,16 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 http.createServer(function (req, res) {
-  fs.readFile('HTML/Index.html', function(err, data) {
+	var path = url.parse(req.url).pathname;
+	if(path == "/getstring")
+	{
+		console.log("request ist da");
+	}
+  	fs.readFile('HTML/Index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
+    
   });
 }).listen(8080);
