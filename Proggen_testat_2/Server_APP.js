8970edeafb506dca31546a621bc557837f6b962f
BKMaster94 <bruno.branco2@web.de>
2017-06-13 18:11:25 +0200

    blaas
    asd

--- Proggen_testat_2/Server_APP.js
+++ Proggen_testat_2/Server_APP.js
@@ -1,13 +1,27 @@
 var express = require('express');
-var app = express();
+var appServer = express();
+var bodyParser = require('body-parser')
+var path = require('path');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+appServer.use(bodyParser.urlencoded(
+		{
+			extended: true 
+			}));
+
+appServer.get('/', function(req,res){
+	res.sendFile(path.resolve('HTML/test.html'));
+});
+
+appServer.post('/test', function (req,res){
+	
+	console.log("Request bekommen");
+	res.send("WUHU");
+	
+});
+
+
+
+appServer.listen(8081, function(){
+	console.log("Server Start on PORT 8081");
+});
