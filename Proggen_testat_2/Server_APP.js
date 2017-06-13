b87886601ac9b066b24d575c8ef1aec0599d030e
BKMaster94 <bruno.branco2@web.de>
2017-06-13 19:44:44 +0200

    Aasfjsdfghb

--- Proggen_testat_2/Server_APP.js
+++ Proggen_testat_2/Server_APP.js
@@ -2,22 +2,42 @@ var express = require('express');
 var appServer = express();
 var bodyParser = require('body-parser')
 var path = require('path');
+var test = "Server Daten";
 
+var abfangen = "";
 
 appServer.use(bodyParser.urlencoded(
 		{
 			extended: true 
 			}));
 
+
+
+
+
 appServer.get('/', function(req,res){
 	res.sendFile(path.resolve('HTML/test.html'));
 });
 
+appServer.get('/Ajax/testAuslagerung.js', function(req,res){
+	res.sendFile(path.resolve('Ajax/testAuslagerung.js'));
+});
+
+
+
 appServer.post('/test', function (req,res){
-	
+	console.log(req.body);
+	abfangen = req.body;
+	console.log(abfangen);
 	console.log("Request bekommen");
+	
 	res.send("WUHU");
 	
+});
+
+appServer.get('/get', function(req,res){
+	res.send(test);
+	console.log("Daten gesendet" + test);
 });
 
 
