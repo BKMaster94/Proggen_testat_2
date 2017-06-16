8b66a80b162a6a88c5d8ee09eeb311c8403d5b98
adriano1997 <adriankraw1@googlemail.com>
2017-06-16 04:08:19 +0200

    Die Spieler Spielen jetzt abwechselnd...viele Abfragen&CSS aulagerung

--- Proggen_testat_2/Ajax/testAuslagerung.js
+++ Proggen_testat_2/Ajax/testAuslagerung.js
@@ -36,6 +36,83 @@ $("#AnmeldeButton").click(function(){ //Wenn der Button wird geklickt und die Fu
 		}
 	});
 });
+var socket = io();
+
+//Wenn diese Fielder clicked werden emmite ich zum server
+// Vom Server kommt die der Call um das Feld mit dem im server festgelegten Zeichen zu markieren
+$("#S1Z_Eins").click(function(){
+//	alert("S1Z_Eins");
+	socket.emit('S1Z_Eins');
+});
+socket.on('S1Z_EinsEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S1Z_Eins').append(zeichen);
+});
+$("#S1Z_Zwei").click(function(){
+//	alert("S1Z_Zwei");
+	socket.emit('S1Z_Zwei');
+});
+socket.on('S1Z_ZweiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S1Z_Zwei').append(zeichen);
+});
+$("#S1Z_Drei").click(function(){
+//	alert("S1Z_Drei");
+	socket.emit('S1Z_Drei');
+});
+socket.on('S1Z_DreiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S1Z_Drei').append(zeichen);
+});
+$("#S2Z_Eins").click(function(){
+//	alert("S1Z_Eins");
+	socket.emit('S2Z_Eins');
+});
+socket.on('S2Z_EinsEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S2Z_Eins').append(zeichen);
+});
+$("#S2Z_Zwei").click(function(){
+//	alert("S1Z_Zwei");
+	socket.emit('S2Z_Zwei');
+});
+socket.on('S2Z_ZweiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S2Z_Zwei').append(zeichen);
+});
+$("#S2Z_Drei").click(function(){
+//	alert("S1Z_Drei");
+	socket.emit('S2Z_Drei');
+});
+socket.on('S2Z_DreiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S2Z_Drei').append(zeichen);
+});
+$("#S3Z_Eins").click(function(){
+//	alert("S1Z_Eins");
+	socket.emit('S3Z_Eins');
+});
+socket.on('S3Z_EinsEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S3Z_Eins').append(zeichen);
+});
+$("#S3Z_Zwei").click(function(){
+//	alert("S1Z_Zwei");
+	socket.emit('S3Z_Zwei');
+});
+socket.on('S3Z_ZweiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S3Z_Zwei').append(zeichen);
+});
+$("#S3Z_Drei").click(function(){
+//	alert("S1Z_Drei");
+	socket.emit('S3Z_Drei');
+});
+socket.on('S3Z_DreiEmiter', function(zeichen){
+//	alert("wieder beim Client");
+	$('#S3Z_Drei').append(zeichen);
+});
+
 
 //$("#jsonClick").click(function(){
 //	alert("JSON Objekt kommt");
--- Proggen_testat_2/Server_APP.js
+++ Proggen_testat_2/Server_APP.js
@@ -17,6 +17,9 @@ var usernameForGame1 = "empty";
 var spieler1= "";
 var spieler2= "";
 
+var zeichenXY = "";//Das Zeichen "X" oder "Y" im normalfall
+var zuege = 0;//anzahl der schon gemachten Zuege
+
 /*fs.writeFile(path.resolve('JSON/spielerName.json'), JSON.stringify(playerName), (err) => { // Test zum erstellen von datein.
 	  if (err) throw err;
 	  console.log('The file has been saved!');
@@ -42,11 +45,13 @@ appServer.use(bodyParser.urlencoded({ // brauchen wir für das parsen vom Post b
 				client.join('SpielRaum');
 				//console.log("Client: " + client.username + " ist nun im SpieleRaum");
 				if (clientCount === 1){
-					spieler1 = client.username; 
+					spieler1 = client.username;
+					client.id = 0; //ADI   Die Id des ersten clients wird auf 0 gesetzt
 					console.log (spieler1);
 				}
 				if(clientCount === 2){
 					spieler2 = client.username;
+					client.id=1;//ADI   Die Id des ersten clients wird auf 1 gesetzt
 					console.log(spieler2);
 					io.to('SpielRaum').emit('SpielerName1', spieler1);
 					io.to('SpielRaum').emit('SpielerName2', spieler2);
@@ -54,6 +59,7 @@ appServer.use(bodyParser.urlencoded({ // brauchen wir für das parsen vom Post b
 				io.to('SpielRaum').emit('gameReady', clientCount);
 
 			}
+			
 			console.log('A user is Connectet: ' + client.username + " | ID: " + client.id);
 			client.emit('message', 'You are Connected');
 			client.on('disconnect', function(){
@@ -63,8 +69,137 @@ appServer.use(bodyParser.urlencoded({ // brauchen wir für das parsen vom Post b
 			});
 			console.log("Momentane user: " + clientCount);
 	    });
-	});
+		
 
+		//Wurde Geklicked und nun wird ein Zeichen zum client gescchickt
+		// VIELE ABFRAGEN jedes Feld hat seine eigene abfrage
+		client.on('S1Z_Eins', function(){//Es wurde das Feld .. clicked
+			
+		if(client.id === zuege%2){ //ADI  wenn die id des Spielers gleich der zuege modulo 2 ist... somit immer abwechselnd
+//			console.log("S1Z_Eins im server angekommen");
+			io.emit('S1Z_EinsEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S1Z_Zwei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S1Z_Zwei im server angekommen");
+			io.emit('S1Z_ZweiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S1Z_Drei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S1Z_Drei im server angekommen");
+			io.emit('S1Z_DreiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S2Z_Eins', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S2Z_Eins im server angekommen");
+			io.emit('S2Z_EinsEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S2Z_Zwei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S2Z_Zwei im server angekommen");
+			io.emit('S2Z_ZweiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S2Z_Drei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S2Z_Drei im server angekommen");
+			io.emit('S2Z_DreiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S3Z_Eins', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S3Z_Eins im server angekommen");
+			io.emit('S3Z_EinsEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S3Z_Zwei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S3Z_Zwei im server angekommen");
+			io.emit('S3Z_ZweiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+		client.on('S3Z_Drei', function(){//Es wurde das Feld .. clicked
+
+		if(client.id === zuege%2){
+//			console.log("S3Z_Drei im server angekommen");
+			io.emit('S3Z_DreiEmiter', zeichenXY);
+			if(zuege%2===0){
+				zeichenXY = "X";
+			}
+			else{
+				zeichenXY = "O";
+			}
+			zuege++;
+		}
+		});
+	});
 // Hier endet Socket.io
 
 appServer.get('/', function(req,res){ // Url Abfangen
@@ -85,6 +220,10 @@ appServer.get('/Ajax/testAuslagerung.js', function(req,res){ // URL Abfangen
 	res.sendFile(path.resolve('Ajax/testAuslagerung.js')); // Lade die Javascript datei ein.
 });
 
+appServer.get('/CSS/StyleIndex.css', function(req,res){ // URL Abfangen
+	res.sendFile(path.resolve('CSS/StyleIndex.css')); // Lade die CSS datei ein.
+});
+
 appServer.post('/JsonPlayerNameBekommen', function (req,res){ //der SpielerName wird durch ein JSON an den server geschickt
 	abfangen = req.body; // Nehme das Object aus der Request und packe sie in eine Variable.
 	console.log(abfangen); //In der Console sehen wir die variable, welche das Object enthält
@@ -102,6 +241,8 @@ appServer.get('/get', function(req,res){ // Get Methode
 	res.send(test); // Sende dem Client informationen
 	console.log("Daten gesendet" + test);
 });
+
+
 
 //appServer.listen(8081, function(){ // Höre auf den Port 8081
 //	console.log("Server Start on PORT 8081"); // Server console Ausgabe
